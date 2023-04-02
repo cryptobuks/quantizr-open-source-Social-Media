93114fec8476f3eef3ed0ffeb089c7ac62599636
Clay Ferguson <wclayf@gmail.com>
2023-04-02 01:14:40 -0500

    css generator fix (wildcard in path failing)

--- scripts/gen-css-imports.sh
+++ scripts/gen-css-imports.sh
@@ -9,3 +9,8 @@ echo "// WARNING: Auto-generated File (see build.sh)" > imports.scss
 printf "@import '%s';\n" ./imports/*.scss >> imports.scss
 printf "@import '%s';\n" ../ts/**/*.scss >> imports.scss
 
+# NOTE: Apparently there's a bug in printf command because it doesn't seem like I should need
+# to supply more than one "**" a directory to get full recursion but it's not working unless I put
+# all these deeper wildcards
+printf "@import '%s';\n" ../ts/**/**/*.scss >> imports.scss
+
