492cacc0b0e4e2c2b71072cac66de980e1321e30
Clay Ferguson <wclayf@gmail.com>
2023-05-22 16:50:07 -0500

    formatDate moved to webpack common

--- src/main/resources/public/webpack.dev.js
+++ src/main/resources/public/webpack.dev.js
@@ -2,18 +2,7 @@ const webpack = require("webpack");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 
-// todo-2: need to export this out of common
-function formatDate(date) {
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    var ampm = hours >= 12 ? "pm" : "am";
-    hours = hours % 12;
-    hours = hours ? hours : 12; // the hour '0' should be '12'
-    var strTime = hours + ":" + (minutes < 10 ? "0" + minutes : minutes) + ampm;
-    return (date.getMonth() + 1) + "-" + date.getDate() + "-" + date.getFullYear() + " " + strTime;
-}
-
-module.exports = merge(common, {
+module.exports = merge(common.config, {
     mode: "development",
     devtool: "source-map",
     plugins: [
@@ -21,7 +10,7 @@ module.exports = merge(common, {
             // this was a test: Didn't work. I tried it in the index.html and it was not translated.
             // Ended up accomplishing this using my 'cachebuster' option on HtmlWebpackPlugin instead.
             // WARNING: The 'stringify' here looks redundant but it's actually requird here by DefinePlugin
-            BUILDTIME: JSON.stringify(formatDate(new Date())),
+            BUILDTIME: JSON.stringify(common.formatDate(new Date())),
             PROFILE: JSON.stringify("dev")
         })
     ]
